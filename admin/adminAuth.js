57b0c42166021d5a328f3e5ff15206486cb328a2
makariosemporium <makariosemporium@gmail.com>
2025-12-12 12:45:55 +0800

    Update adminAuth.js

--- admin/adminAuth.js
+++ admin/adminAuth.js
@@ -1,16 +1,14 @@
 // adminAuth.js
-import { auth, db, onAuthStateChanged, doc, getDoc } from "../firebase.js";
+import { auth, onAuthStateChanged } from "../firebase.js";
 
 const ADMIN_UID = "0lRoMun3CXTwhfUp5W0PmC0j7Cz2"; // Your admin UID
 
-onAuthStateChanged(auth, async user => {
+onAuthStateChanged(auth, user => {
   if (!user) {
-    // Not logged in → redirect
     window.location.href = "../auth/login.html";
     return;
   }
 
-  // Check if the logged-in user's UID matches the admin UID
   if (user.uid !== ADMIN_UID) {
     alert("Access denied. Admins only.");
     auth.signOut();
